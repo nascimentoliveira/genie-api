909b42d5b110b53d10663ffdbd2bd18230a953c5
Thiago Nascimento <nascimentoliveira@gmail.com>
2023-04-07 10:57:00 -0300

    Add @prisma/client as a dependency and configure Prisma schema and database connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import cors from "cors";
+import { connectDb, disconnectDB } from "./config";
 
 const app = express();
 app
@@ -9,10 +10,12 @@ app
 
 
 export function init(): Promise<Express> {
+  connectDb();
   return Promise.resolve(app);
 }
 
 export async function close(): Promise<void> {
+  await disconnectDB();
 }
 
 export default app;
