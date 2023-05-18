3b3d004034c5e766bd1bd1a50edcffa195d809de
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-18 20:04:57 -0300

    update package.json

--- src/utils/prisma-utils.ts
+++ src/utils/prisma-utils.ts
@@ -1,4 +1,4 @@
-export function exclude<T, Key extends keyof T>(entity: T, ...keys: Key[]): Omit<T, Key> {
+export function exclude <T, Key extends keyof T> (entity: T, ...keys: Key[]): Omit<T, Key> {
   const newEntity = JSON.parse(JSON.stringify(entity));
   for (const key of keys) {
     delete newEntity[key];
